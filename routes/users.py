95fed55ac0b60d84a2334527251acf7b009c9099
Rajeswari Subramanian <rajeswarisubramanian1772@gmail.com>
2025-10-04 14:01:16 -0500

    initial code

---
+++ routes/users.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from models import User
+from db_config import users_col
+
+router = APIRouter()
+
+@router.post("/register")
+def register_user(user: User):
+    users_col.insert_one(user.dict())
+    return {"message": "User registered successfully!"}
+
+@router.get("/users")
+def get_all_users():
+    users = list(users_col.find({}, {"_id": 0}))
+    return {"users": users}
